e9d942e1be58330543d0cd9131b25b7d942d0998
abuzar-gifari <shaikh35-3024@diu.edu.bd>
2022-05-07 12:57:18 +0600

    skydash admin template integrated with our project files

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\admin\AdminController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,3 +23,6 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+// OUR CUSTOM ROUTES
+Route::get('admin/dashboard',[AdminController::class,'dashboard']);
